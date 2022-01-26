805dd467e5ddb67e8e9d6af24f3fce09164c9513
Eugeeun <97161495+Eugeeun@users.noreply.github.com>
2022-01-26 23:08:47 +0900

    JS navbar menu click

--- main.js
+++ main.js
@@ -5,10 +5,23 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-  console.log(`navbarHeight: ${navbarHeight}`);
+  // console.log(`navbarHeight: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', event => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (!link) return;
+  // 링크가 없다면 early return
+  else console.log(event.target.dataset.link);
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
