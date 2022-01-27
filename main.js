0b07339b57d31df551ec819eaad497920fbb2f52
Eugeeun <97161495+Eugeeun@users.noreply.github.com>
2022-01-27 23:47:08 +0900

    JS project filtering

--- main.js
+++ main.js
@@ -51,6 +51,35 @@ toTopBtn.addEventListener('click', () => {
   scrollIntoView('#home');
 });
 
+// do filtering when btn is clicked
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', event => {
+  const filter =
+    event.target.dataset.filter || event.target.parentNode.dataset.filter;
+  // 클릭한 곳에 데이터가 들어있으면 그것을 쓰고 없으면 부모노드로 가서 가져옴
+
+  if (!filter) return;
+
+  projectContainer.classList.add('ani-out');
+  setTimeout(() => {
+    projects.forEach(project => {
+      // console.log(project.dataset.type);
+      if (filter === 'all' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    // 필터링을 수행하고 이제 다시 나타나야 됨
+    projectContainer.classList.remove('ani-out');
+    // 제거해줌
+    // 그래서 필터링이 안된 채로 사라지고 필터링이 된 채로 나타남!
+  }, 300); // 이 코드를 0.3초 후에 실행해줘
+  // console.log(filter);
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
