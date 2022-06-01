d8b8abbdaf84ddacab49b57ba70ef2cb7b05e22a
Eugeeun <97161495+Eugeeun@users.noreply.github.com>
2022-06-01 13:19:43 +0900

    remove js folder

--- js/background.js
+++
@@ -1,7 +0,0 @@
-const backgroundImgCnt = 5;
-const chosenImg = Math.floor(Math.random() * backgroundImgCnt) + 1;
-console.log(chosenImg);
-
-document.getElementById(
-  'home'
-).style.backgroundImage = `url(imgs/backgrounds/background${chosenImg}.jpg)`;
